4fc9a1db5a8a348b85f5837a6668a1f2fca96c25
KIm <fenderst0612@gmail.com>
2021-05-04 18:59:47 +0900

    add: REST 実装

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/rightcode-tutorial
 go 1.15
 
 require (
-	cloud.google.com/go/firestore v1.5.0 // indirect
+	cloud.google.com/go/firestore v1.5.0
 	firebase.google.com/go v3.13.0+incompatible
 	google.golang.org/api v0.46.0
 )
--- main.go
+++ main.go
@@ -5,13 +5,59 @@ import (
 	"fmt"
 	"log"
 
+	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
+
+type User struct {
+	first  string
+	middle string
+	last   string
+	born   int
+}
 
 func main() {
 	fmt.Println("Hello World!!")
 
+	// addUser()
+	// updataUser()
+	deleteUser()
+	readUser()
+
+}
+
+func readUser() {
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("path/to/serviceAccount.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	iter := client.Collection("users").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			if err == iterator.Done {
+				break
+			}
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Println(doc.Data())
+	}
+}
+
+func addUser() {
 	// Use a service account
 	ctx := context.Background()
 	sa := option.WithCredentialsFile("path/to/serviceAccount.json")
@@ -20,12 +66,89 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	// create Client
+	// Create Client
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	// データ追加
+
+	// user1 := User{
+	// 	first: "Ada",
+	// 	last: "Lovelance",
+	// 	born: 1815,
+	// }
+
+	// user2 := User{
+	// 	first:  "Ada",
+	// 	middle: "Mathison",
+	// 	last:   "Lovelace",
+	// 	born:   1815,
+	// }
+
+	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+		"first": "Ada",
+		"last":  "Lovelace",
+		"born":  1815,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding alovelace: %v", err)
+	}
+}
+
+func updataUser() {
+	// 初期化
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("path/to/serviceAccount.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	client, err := app.Firestore(ctx)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	defer client.Close()
+	// データ更新
+	_, updateError := client.Collection("users").Doc("user2").Set(ctx, map[string]interface{}{
+		"first":  "Yeah",
+		"middle": "deleted items",
+	}, firestore.MergeAll)
+	if updateError != nil {
+		// Handle any errors in an appropriate way, such as returning them.
+		log.Printf("An error has occurred: %s", err)
+	}
 
+	// 切断
+	defer client.Close()
+}
+
+func deleteUser() {
+	// 初期化
+	ctx := context.Background()
+	sa := option.WithCredentialsFile("path/to/serviceAccount.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// フィールド削除
+	_, errorDelete := client.Collection("users").Doc("user2").Update(ctx, []firestore.Update{
+		{
+			Path:  "middle",
+			Value: firestore.Delete,
+		},
+	})
+	if errorDelete != nil {
+		// Handle any errors in an appropriate way, such as returning them.
+		log.Printf("An error has occurred: %s", err)
+	}
 }
